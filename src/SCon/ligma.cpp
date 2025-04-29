7525a1ed4ad7b6bee9242e6dafce30a33f8c6d81
makredzic <mak.redzic@gmail.com>
2025-04-29 22:47:25 +0200

    Removed code and replaced it with coroutine example

--- src/SCon/ligma.cpp
+++ src/SCon/ligma.cpp
@@ -1,4 +1,3 @@
-#include <SCon/scope.hpp>
 
 
 namespace SCon {
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
-#include <string>
+#include <coroutine>
 
-int main(){
+struct ReturnObject {
+
+    struct promise_type {
+        ReturnObject get_return_object() { return ReturnObject{std::coroutine_handle<promise_type>::from_promise(*this)}; }
+        std::suspend_never initial_suspend() { return {}; }
+        std::suspend_always final_suspend() noexcept { return {}; }
+        void return_void() {}
+        void unhandled_exception() {}
+    };
+
+    std::coroutine_handle<> handle;
+    ReturnObject(std::coroutine_handle<> handle_): handle{handle_} {}
+
+};
 
 
-    std::thread
+ReturnObject foo() {
+    std::cout << "1. Hello from foo\n";
+
+    co_await std::suspend_always{};
+
+    std::cout << "2. Hello again from foo\n";
+}
 
 
 
+int main() {
+
+    ReturnObject retObj = foo();
+    std::cout << "coroutine is done? = " << retObj.handle.done() << std::endl;
+
+    retObj.handle.resume();
+    std::cout << "coroutine is done? = " << retObj.handle.done() << std::endl;
+
+    // retObj.handle.resume();
+    // std::cout << "coroutine is done? = " << retObj.handle.done() << std::endl;
     return 0;
 }
